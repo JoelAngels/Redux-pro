264d018c35ff8c2dec9aff69cd992a6f8d0d6f8b
Joel Angel <joelbaraka4325@gmail.com>
2023-01-24 19:24:50 +0300

    cake and ice app done

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,5 +1,9 @@
 const store = require("./app/store");
+
+//import actions here
 const cakeActions = require("./app/features/cake/cakeSlice").cakeActions;
+const icecreamActions =
+  require("./app/features/icecream/icecreamSlice").icecreamActions;
 
 console.log("Initial State", store.getState());
 
@@ -12,5 +16,9 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
 
 unsubscribe();
